d44dc7c136701699670f2f5b009124d265efbdb7
Ben Vaughn <67301682+cplhicks82@users.noreply.github.com>
2020-06-24 21:12:29 -0600

    world_map
    Create hover text, customized markers, automate title

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,24 +4,35 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
 	all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
+chart_title = all_eq_data['metadata']['title']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-	mag = eq_dict['properties']['mag']
-	lon = eq_dict['geometry']['coordinates'][0]
-	lat= eq_dict['geometry']['coordinates'][1]
-	mags.append(mag)
-	lons.append(lon)
-	lats.append(lat)
+	mags.append(eq_dict['properties']['mag'])
+	lons.append(eq_dict['geometry']['coordinates'][0])
+	lats.append(eq_dict['geometry']['coordinates'][1])
+	hover_texts.append(eq_dict['properties']['title'])
 
 # Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]
-my_layout = Layout(title='Global Earthquakes')
+data = [{
+	'type': 'scattergeo',
+	'lon': lons,
+	'lat': lats,
+	'text': hover_texts,
+	'marker': {
+		'size': [3.5*mag for mag in mags],
+		'color': mags,
+		'colorscale': 'Viridis',
+		'reversescale': True,
+		'colorbar': {'title': 'Magnitude'},
+	},
+}]
+my_layout = Layout(title=chart_title)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
